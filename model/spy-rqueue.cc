cb334b860486cf6cfafafdf4994e227586e16a40
ojoaosoares <ojoaosoaressantos@gmail.com>
2025-06-19 01:26:37 -0300

    Renaming receive queue header and implementantion.

--- model/spy-rqueue.cc
+++ model/spy-rqueue.cc
@@ -1,128 +1,122 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
-#include "gpsr-rqueue.h"
+#include "spy-rqueue.h"
 #include <algorithm>
 #include <functional>
 #include "ns3/ipv4-route.h"
 #include "ns3/socket.h"
 #include "ns3/log.h"
 
-NS_LOG_COMPONENT_DEFINE ("GpsrRequestQueue");
+NS_LOG_COMPONENT_DEFINE ("SpyRequestQueue");
 
 namespace ns3 {
-namespace gpsr {
-uint32_t
-RequestQueue::GetSize ()
-{
-  Purge ();
-  return m_queue.size ();
-}
+  namespace spy {
 
-bool
-RequestQueue::Enqueue (QueueEntry & entry)
-{
-  Purge ();
-  for (std::vector<QueueEntry>::const_iterator i = m_queue.begin (); i
-       != m_queue.end (); ++i)
+    uint32_t RequestQueue::GetSize ()
     {
-      if ((i->GetPacket ()->GetUid () == entry.GetPacket ()->GetUid ())
-          && (i->GetIpv4Header ().GetDestination ()
-              == entry.GetIpv4Header ().GetDestination ()))
+      Purge ();
+      return m_queue.size ();
+    }
+
+    bool RequestQueue::Enqueue (QueueEntry & entry)
+    {
+      Purge ();
+      for (std::vector<QueueEntry>::const_iterator i = m_queue.begin (); i
+          != m_queue.end (); ++i)
         {
-          return false;
+          if ((i->GetPacket ()->GetUid () == entry.GetPacket ()->GetUid ())
+              && (i->GetIpv4Header ().GetDestination ()
+                  == entry.GetIpv4Header ().GetDestination ()))
+            {
+              return false;
+            }
         }
-    }
-  entry.SetExpireTime (m_queueTimeout);
-  if (m_queue.size () == m_maxLen)
-    {
-      Drop (m_queue.front (), "Drop the most aged packet");     // Drop the most aged packet
-      m_queue.erase (m_queue.begin ());
-    }
-  m_queue.push_back (entry);
-  return true;
-}
-
-void
-RequestQueue::DropPacketWithDst (Ipv4Address dst)
-{
-  NS_LOG_FUNCTION (this << dst);
-  Purge ();
-//  const Ipv4Address addr = dst;
-  for (std::vector<QueueEntry>::iterator i = m_queue.begin (); i
-       != m_queue.end (); ++i)
-    {
-      if (IsEqual (*i, dst))
+      entry.SetExpireTime (m_queueTimeout);
+      if (m_queue.size () == m_maxLen)
         {
-          Drop (*i, "DropPacketWithDst ");
+          Drop (m_queue.front (), "Drop the most aged packet");     // Drop the most aged packet
+          m_queue.erase (m_queue.begin ());
         }
+      m_queue.push_back (entry);
+      return true;
     }
-  m_queue.erase (std::remove_if (m_queue.begin (), m_queue.end (),
-                                 std::bind2nd (std::ptr_fun (RequestQueue::IsEqual), dst)), m_queue.end ());
-}
 
-bool
-RequestQueue::Dequeue (Ipv4Address dst, QueueEntry & entry)
-{
-  Purge ();
-  for (std::vector<QueueEntry>::iterator i = m_queue.begin (); i != m_queue.end (); ++i)
+    void RequestQueue::DropPacketWithDst (Ipv4Address dst)
     {
-      if (i->GetIpv4Header ().GetDestination () == dst)
+      NS_LOG_FUNCTION (this << dst);
+      Purge ();
+    //  const Ipv4Address addr = dst;
+      for (std::vector<QueueEntry>::iterator i = m_queue.begin (); i
+          != m_queue.end (); ++i)
         {
-          entry = *i;
-          m_queue.erase (i);
-          return true;
+          if (IsEqual (*i, dst))
+            {
+              Drop (*i, "DropPacketWithDst ");
+            }
         }
+      m_queue.erase (std::remove_if (m_queue.begin (), m_queue.end (),
+                                    std::bind2nd (std::ptr_fun (RequestQueue::IsEqual), dst)), m_queue.end ());
     }
-  return false;
-}
 
-bool
-RequestQueue::Find (Ipv4Address dst)
-{
-  for (std::vector<QueueEntry>::const_iterator i = m_queue.begin (); i
-       != m_queue.end (); ++i)
+    bool RequestQueue::Dequeue (Ipv4Address dst, QueueEntry & entry)
     {
-      if (i->GetIpv4Header ().GetDestination () == dst)
+      Purge ();
+      for (std::vector<QueueEntry>::iterator i = m_queue.begin (); i != m_queue.end (); ++i)
         {
-          return true;
+          if (i->GetIpv4Header ().GetDestination () == dst)
+            {
+              entry = *i;
+              m_queue.erase (i);
+              return true;
+            }
         }
+      return false;
     }
-  return false;
-}
 
-struct IsExpired
-{
-  bool
-  operator() (QueueEntry const & e) const
-  {
-    return (e.GetExpireTime () < Seconds (0));
+    bool RequestQueue::Find (Ipv4Address dst)
+    {
+      for (std::vector<QueueEntry>::const_iterator i = m_queue.begin (); i
+          != m_queue.end (); ++i)
+        {
+          if (i->GetIpv4Header ().GetDestination () == dst)
+            {
+              return true;
+            }
+        }
+      return false;
+    }
+
+    struct IsExpired
+    {
+      bool
+      operator() (QueueEntry const & e) const
+      {
+        return (e.GetExpireTime () < Seconds (0));
+      }
+    };
+
+    void RequestQueue::Purge ()
+    {
+      IsExpired pred;
+      for (std::vector<QueueEntry>::iterator i = m_queue.begin (); i
+          != m_queue.end (); ++i)
+        {
+          if (pred (*i))
+            {
+              Drop (*i, "Drop outdated packet ");
+            }
+        }
+      m_queue.erase (std::remove_if (m_queue.begin (), m_queue.end (), pred),
+                    m_queue.end ());
+    }
+
+    void RequestQueue::Drop (QueueEntry en, std::string reason)
+    {
+      NS_LOG_LOGIC (reason << en.GetPacket ()->GetUid () << " " << en.GetIpv4Header ().GetDestination ());
+      en.GetErrorCallback () (en.GetPacket (), en.GetIpv4Header (),
+                              Socket::ERROR_NOROUTETOHOST);
+      return;
+    }
+
   }
-};
-
-void
-RequestQueue::Purge ()
-{
-  IsExpired pred;
-  for (std::vector<QueueEntry>::iterator i = m_queue.begin (); i
-       != m_queue.end (); ++i)
-    {
-      if (pred (*i))
-        {
-          Drop (*i, "Drop outdated packet ");
-        }
-    }
-  m_queue.erase (std::remove_if (m_queue.begin (), m_queue.end (), pred),
-                 m_queue.end ());
-}
-
-void
-RequestQueue::Drop (QueueEntry en, std::string reason)
-{
-  NS_LOG_LOGIC (reason << en.GetPacket ()->GetUid () << " " << en.GetIpv4Header ().GetDestination ());
-  en.GetErrorCallback () (en.GetPacket (), en.GetIpv4Header (),
-                          Socket::ERROR_NOROUTETOHOST);
-  return;
-}
-
-}
 }
