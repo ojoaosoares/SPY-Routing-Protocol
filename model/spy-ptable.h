536412e20506bfbb27ec93910a69d6f3221ce60d
ojoaosoares <ojoaosoaressantos@gmail.com>
2025-06-19 00:09:24 -0300

    Renaming pos table header and implementantion.

--- model/spy-ptable.h
+++ model/spy-ptable.h
@@ -1,5 +1,5 @@
-#ifndef GPSR_PTABLE_H
-#define GPSR_PTABLE_H
+#ifndef SPY_PTABLE_H
+#define SPY_PTABLE_H
 
 #include <map>
 #include <cassert>
@@ -16,110 +16,104 @@
 #include <complex>
 
 namespace ns3 {
-namespace gpsr {
+  namespace spy {
 
-/*
- * \ingroup gpsr
- * \brief Position table used by GPSR
- */
-class PositionTable
-{
-public:
-  /// c-tor
-  PositionTable ();
+    /*
+    * \ingroup spy
+    * \brief Position table used by SPY
+    */
+    class PositionTable
+    {
+      public:
+        /// c-tor
+        PositionTable ();
 
-  /**
-   * \brief Gets the last time the entry was updated
-   * \param id Ipv4Address to get time of update from
-   * \return Time of last update to the position
-   */
-  Time GetEntryUpdateTime (Ipv4Address id);
+        /**
+         * \brief Gets the last time the entry was updated
+         * \param id Ipv4Address to get time of update from
+         * \return Time of last update to the position
+         */
+        Time GetEntryUpdateTime (Ipv4Address id);
 
-  /**
-   * \brief Adds entry in position table
-   */
-  void AddEntry (Ipv4Address id, Vector position);
+        /**
+         * \brief Adds entry in position table
+         */
+        void AddEntry (Ipv4Address id, Vector position);
 
-  /**
-   * \brief Deletes entry in position table
-   */
-  void DeleteEntry (Ipv4Address id);
+        /**
+         * \brief Deletes entry in position table
+         */
+        void DeleteEntry (Ipv4Address id);
 
-  /**
-   * \brief Gets position from position table
-   * \param id Ipv4Address to get position from
-   * \return Position of that id or NULL if not known
-   */
-  Vector GetPosition (Ipv4Address id);
+        /**
+         * \brief Gets position from position table
+         * \param id Ipv4Address to get position from
+         * \return Position of that id or NULL if not known
+         */
+        Vector GetPosition (Ipv4Address id);
 
-  /**
-   * \brief Checks if a node is a neighbour
-   * \param id Ipv4Address of the node to check
-   * \return True if the node is neighbour, false otherwise
-   */
-  bool isNeighbour (Ipv4Address id);
+        /**
+         * \brief Checks if a node is a neighbour
+         * \param id Ipv4Address of the node to check
+         * \return True if the node is neighbour, false otherwise
+         */
+        bool isNeighbour (Ipv4Address id);
 
-  /**
-   * \brief remove entries with expired lifetime
-   */
-  void Purge ();
+        /**
+         * \brief remove entries with expired lifetime
+         */
+        void Purge ();
 
-  /**
-   * \brief clears all entries
-   */
-  void Clear ();
+        /**
+         * \brief clears all entries
+         */
+        void Clear ();
 
-  /**
-   * \Get Callback to ProcessTxError
-   */
-  Callback<void, WifiMacHeader const &> GetTxErrorCallback () const
-  {
-    return m_txErrorCallback;
-  }
+        /**
+         * \Get Callback to ProcessTxError
+         */
+        Callback<void, WifiMacHeader const &> GetTxErrorCallback () const
+        {
+          return m_txErrorCallback;
+        }
 
-  /**
-   * \brief Gets next hop according to GPSR protocol
-   * \param position the position of the destination node
-   * \param nodePos the position of the node that has the packet
-   * \return Ipv4Address of the next hop, Ipv4Address::GetZero () if no nighbour was found in greedy mode
-   */
-  Ipv4Address BestNeighbor (Vector position, Vector nodePos);
+        /**
+         * \brief Gets next hop according to SPY protocol
+         * \param position the position of the destination node
+         * \param nodePos the position of the node that has the packet
+         * \return Ipv4Address of the next hop, Ipv4Address::GetZero () if no nighbour was found in greedy mode
+         */
+        Ipv4Address BestNeighbor (Vector position, Vector nodePos);
 
-  bool IsInSearch (Ipv4Address id);
+        bool IsInSearch (Ipv4Address id);
 
-  bool HasPosition (Ipv4Address id);
+        bool HasPosition (Ipv4Address id);
 
-  static Vector GetInvalidPosition ()
-  {
-    return Vector (-1, -1, 0);
-  }
+        static Vector GetInvalidPosition ()
+        {
+          return Vector (-1, -1, 0);
+        }
 
-  /**
-   * \brief Gets next hop according to GPSR recovery-mode protocol (right hand rule)
-   * \param previousHop the position of the node that sent the packet to this node
-   * \param nodePos the position of the destination node
-   * \return Ipv4Address of the next hop, Ipv4Address::GetZero () if no nighbour was found in greedy mode
-   */
-  Ipv4Address BestAngle (Vector previousHop, Vector nodePos);
+        /**
+         * \brief Gets next hop according to SPY recovery-mode protocol (right hand rule)
+         * \param previousHop the position of the node that sent the packet to this node
+         * \param nodePos the position of the destination node
+         * \return Ipv4Address of the next hop, Ipv4Address::GetZero () if no nighbour was found in greedy mode
+         */
+        Ipv4Address BestAngle (Vector previousHop, Vector nodePos);
 
-  //Gives angle between the vector CentrePos-Refpos to the vector CentrePos-node counterclockwise
-  double GetAngle (Vector centrePos, Vector refPos, Vector node);
+        //Gives angle between the vector CentrePos-Refpos to the vector CentrePos-node counterclockwise
+        double GetAngle (Vector centrePos, Vector refPos, Vector node);
 
+      private:
+        Time m_entryLifeTime;
+        std::map<Ipv4Address, std::pair<Vector, Time> > m_table;
+        // TX error callback
+        Callback<void, WifiMacHeader const &> m_txErrorCallback;
+        // Process layer 2 TX error notification
+        void ProcessTxError (WifiMacHeader const&);
+    };
 
-
-private:
-  Time m_entryLifeTime;
-  std::map<Ipv4Address, std::pair<Vector, Time> > m_table;
-  // TX error callback
-  Callback<void, WifiMacHeader const &> m_txErrorCallback;
-  // Process layer 2 TX error notification
-  void ProcessTxError (WifiMacHeader const&);
-
-
-
-
-};
-
-}   // gpsr
+  }   // spy
 } // ns3
-#endif /* GPSR_PTABLE_H */
+#endif /* SPY_PTABLE_H */
